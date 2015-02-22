1ade5ff6e3feb1bc6e3f5738091b7220ae196bce
Alangi Derick <alangiderick@gmail.com>
2015-02-22 12:23:34 +0100

    Dashboard Creation

--- app/views/back_end/blayout.blade.php
+++ app/views/back_end/blayout.blade.php
@@ -6,16 +6,20 @@
 
     @yield('title');
 
+    <!-- Custom styles for our template -->
+  <link rel="stylesheet" href="{{ asset('css/bootstrap-theme.css') }}" media="screen" >
+  <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+
 	<!-- BOOTSTRAP STYLES-->
-    <link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
      <!-- FONTAWESOME STYLES-->
-    <link href="{{ asset('assets/css/font-awesome.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" />
      <!-- MORRIS CHART STYLES-->
     <link href="{{ asset('assets/js/morris/morris-0.4.3.min.css') }}" rel="stylesheet" />
         <!-- CUSTOM STYLES-->
     <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet" />
      <!-- GOOGLE FONTS-->
-   <link href="{{ asset('http://fonts.googleapis.com/css?family=Open+Sans') }}" rel='stylesheet' type='text/css' />
+   <link href="{{ asset('http://fonts.googleapis.com/css?family=Open+Sans:300,400,700') }}" rel="stylesheet" />
 </head>
 <body>
     <div id="wrapper">
@@ -36,6 +40,83 @@ font-size: 16px;">{{ HTML::link('/logout', 'Logout', array('class' => 'btn btn-i
         </nav> 
 
         @yield('content');
+
+        <footer id="footer" class="top-space">
+
+    <div class="footer1">
+      <div class="container">
+        <div class="row">
+          
+          <div class="col-md-3 widget">
+            <h3 class="widget-title">Contact</h3>
+            <div class="widget-body">
+              <p>+237 678010978<br>
+              <p>+237 651276262<br>
+              <p>+237 678509550<br>
+              <p>+237 671465706<br>
+                <a href="mailto:#">ims_soft400@gmail.com</a><br>
+                <br>
+                Faculty of Engineering, University of Buea
+              </p>  
+            </div>
+          </div>
+
+          <div class="col-md-3 widget">
+            <h3 class="widget-title">Follow me</h3>
+            <div class="widget-body">
+              <p class="follow-me-icons clearfix">
+                <a href=""><i class="fa fa-twitter fa-2"></i></a>
+                <a href=""><i class="fa fa-dribbble fa-2"></i></a>
+                <a href=""><i class="fa fa-github fa-2"></i></a>
+                <a href=""><i class="fa fa-facebook fa-2"></i></a>
+              </p>  
+            </div>
+          </div>
+
+          <div class="col-md-6 widget">
+            <h3 class="widget-title">About Online Inventory Management</h3>
+            <div class="widget-body">
+              <p>Our Sales, Success, and Support Teams are there for every stage of your onboarding onto T
+              radeGecko. Once you start your trial, an Account Manager will call to establish how we can 
+              meet your specific needs and get you started on using our inventory management software. 
+              We are also ready for any question 
+              you might have, and have prepared plenty of tutorials in our Help Center. </p>
+              <p>So you get more than just inventory management software and support, you 
+              gain access to lots of valuable resources on how to better manage your 
+              inventory and improve your wholesale business – now that’s what we call value adding.</p>
+            </div>
+          </div>
+
+        </div> <!-- /row of widgets -->
+      </div>
+    </div>
+
+    <div class="footer2">
+      <div class="container">
+        <div class="row">
+          
+          <div class="col-md-6 widget">
+            <div class="widget-body">
+              <p class="simplenav">
+                {{ HTML::link('/', 'Home') }} | 
+                <a href="about.html">About</a> |
+                <a href="#">Contact</a>
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-6 widget">
+            <div class="widget-body">
+              <p class="text-right">
+                Copyright &copy; 2014, Design by MIRD Team
+              </p>
+            </div>
+          </div>
+
+        </div> <!-- /row of widgets -->
+      </div>
+    </div>
+  </footer> 
 
     <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
     <!-- JQUERY SCRIPTS -->
